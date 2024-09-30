a10a1b68bec31b0361a6f0a4ae7d24cc98007eba
Nei-Kremos <149798485+Nei-Kremos@users.noreply.github.com>
2024-09-30 04:46:40 +0000

    React

---
+++ my-app/app/page.tsx
@@ -0,0 +1,114 @@
+export default function Home() {
+  return (
+    <html>
+    <head>
+        <link rel="stylesheet" href="project.css"></link>
+    </head>
+    <body>
+        <div className="main">
+            <div className="header">
+                <div className="head_text">
+                    <h1>STUDENT'S EVENTS</h1>
+                </div>               
+                <nav>
+                    <ul>
+                        <li>Спорт</li>
+                        <li>Профком</li>
+                        <li>Наука</li>
+                        <li id="choice_btn">Все события</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="selection_h1">
+                <div className="selection_h2">
+                    <div className="sel_point" id="color_1">
+                        <div className="sel_point_head">
+                            <div className="head_text_point">
+                                <p>23.11.2023</p>
+                            </div>
+                            <div className="head_img_point">
+                                <img src="https://img.icons8.com/?size=50&id=9460&format=png"></img>
+                            </div> 
+                        </div>
+                        <div className="none"></div>
+                        <div className="sel_point_foot">
+                            <p>Соревнования по легкой атлетике</p>
+                            <p>Асинхронное ритмическое поле, следовательно, имеет флюгель-горн. Райдер использует хроматический микрохроматический интервал.</p>
+                        </div>
+                    </div>
+    
+                    <div className="sel_point">
+                        <div className="sel_point_head">
+                            <div className="head_text_point">
+                                <p>23.11.2023</p>
+                            </div>
+                            <div className="head_img_point">
+                                <img src="https://img.icons8.com/?size=50&id=9460&format=png"></img>
+                            </div>
+                        </div>
+                        <div className="none"></div>
+                        <div className="sel_point_foot">
+                            <p>Стендап “О науке”</p>
+                            <p>Действительно, нота имитирует неизменный гипнотический рифф. Контрапункт контрастных фактур, на первый взгляд, имитирует рок-н-ролл 50-х. Детройтское техно начинает мелодический рок-н-ролл 50-х.</p>
+                        </div>
+                    </div>
+                        
+                    <div className="sel_point">
+                        <div className="sel_point_head">
+                            <div className="head_text_point">
+                                <p>23.11.2023</p>
+                            </div>
+                            <div className="head_img_point">
+                                <img src="https://img.icons8.com/?size=50&id=9460&format=png"></img>
+                            </div>
+                        </div>
+                        <div className="none"></div>
+                        <div className="sel_point_foot">
+                            <p>КВН</p>
+                            <p>Механическая система, в соответствии с модифицированным уравнением Эйлера, трудна в описании. Крен нелинеен. Отсюда видно, что инерциальная навигация абсолютно требует большего внимания к анализу ошибок, которые даёт механический волчок.</p>
+                        </div>
+                    </div>
+    
+                    <div className="sel_point" id="color_2">
+                        <div className="sel_point_head">
+                            <div className="head_text_point">
+                                <p>23.11.2023</p>
+                            </div>
+                            <div className="head_img_point">
+                                <img src="https://img.icons8.com/?size=50&id=9460&format=png"></img>
+                            </div>
+                        </div>
+                        <div className="none"></div>
+                        <div className="sel_point_foot">
+                            <p>Научная конференция</p>
+                            <p>Малое колебание, согласно уравнениям Лагранжа, безусловно связывает гироинтегратор до полного прекращения вращения.</p>
+                        </div>
+                    </div>
+    
+                    <div className="sel_point">
+                        <div className="sel_point_head">
+                            <div className="head_text_point">
+                                <p>23.11.2023</p>
+                            </div>
+                            <div className="head_img_point">
+                                <img src="https://img.icons8.com/?size=50&id=9460&format=png"></img>
+                            </div>
+                        </div>
+                        <div className="none"></div>
+                        <div className="sel_point_foot">
+                            <p>Волейбол</p>
+                            <p>Исключая малые величины из уравнений, штопор заставляет перейти к более сложной системе дифференциальных уравнений, если добавить лазерный период. Ускорение учитывает вектор угловой скорости.</p>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+            
+            <div className="footer">
+                <p>СГУПС, 2024</p>
+            </div>  
+        </div>  
+    </body>
+</html>
+
+  );
+}
